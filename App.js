053d500e9b92473ef0213a85fe392a6b59c66493
VuDucGiang <ducgiang.nd1808@gmail.com>
2022-04-28 14:01:26 +0700

    search bar

--- App.js
+++ App.js
@@ -6,12 +6,13 @@ import * as Location from 'expo-location';
 
 import TempLocation from './components/TempLocation'
 import WeatherScroll from './components/WeatherScroll'
-import DetailWeatherCurrent from './components/DetailWeatherCurrent';
+import SearchBar from './components/SearchBar';
 const API_KEY ='49cc8c821cd2aff9af04c9f98c36eb74';
 const bg = require('./assets/bg.png')
 export default function App() {
   const [data, setData] = useState({});
   const [location, setLocation] = useState({});
+  const [loaded, setLoaded] = useState(true);
 
   useEffect(() => {
     (async () => {
@@ -26,6 +27,39 @@ export default function App() {
       fetchDataFromApi(location.coords.latitude, location.coords.longitude);
     })();
   }, [])
+
+  async function fetchWeatherData(cityName) {
+    setLoaded(false);
+    const API = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=${API_KEY}`
+    try {
+        const response = await fetch(API);
+        if(response.status == 200) {
+            const location = await response.json();
+            fetchDataFromApi(location.coord.lat, location.coord.lon);
+        } else {
+            setLocation(null);
+        }
+        setLoaded(true);
+        
+    } catch (error) {
+        console.log(error);
+    }
+  }
+  if(!loaded) {
+    return (
+        <View >
+            <Text>Không load được</Text>
+        </View>
+
+    )
+  } else if(location === null) {
+    return (
+        <View style={styles.container}>
+            <SearchBar fetchWeatherData={fetchWeatherData}/>
+            <Text style={styles.primaryText}>City Not Found! Try Different City</Text>
+        </View>
+    )
+    } 
 
 
   const fetchDataFromApi = (latitude, longitude) => {
@@ -46,8 +80,9 @@ export default function App() {
   }
 
   return (
+    
       <ScrollView horizontal = {false} style={styles.container}>
-        
+          <SearchBar fetchWeatherData={fetchWeatherData} />
           <TempLocation current={data.current} timezone={data.timezone} lat={data.lat} lon={data.lon} city = {location.city}/>
           <WeatherScroll weatherData={data.daily}/>
         {/*
